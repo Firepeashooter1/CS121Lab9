98956195baafd14a5e2618ee315f483536c74850
Firepeashooter1 <firepeashooter1@gmail.com>
2025-10-31 01:53:00 +0000

    HasMenu.java commit fixed

--- HasMenu.java
+++ HasMenu.java
@@ -1,12 +1,6 @@
-// HasMenu.java
+
 public interface HasMenu {
-    /**
-     * Returns a string representation of the menu (printed to console by convention).
-     */
     String menu();
 
-    /**
-     * Starts the interactive menu loop for the object.
-     */
     void start();
-}
+}//End public interface HasMenu
